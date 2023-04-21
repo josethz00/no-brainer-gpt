81126b291e26cf7ad8db14b442a19457819cb735
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-21 14:09:34 -0300

    feat: add connect and disconnect to events

--- src/events/execute_backend_event.py
+++ src/events/execute_backend_event.py
@@ -5,6 +5,4 @@ from database.pinecone.vector_db import vector_db
 def execute_backend_server_event_handler(backend_app: fastapi.FastAPI) -> typing.Any:
     async def launch_backend_server_events() -> None:
         vector_db.connect('nobrainer')
-        pass
-
     return launch_backend_server_events
