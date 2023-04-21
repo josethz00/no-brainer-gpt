4d01db443587b7cb8d48f22be273901c3142ea6c
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-21 17:02:06 -0300

    pinecone start connection

--- src/database/pinecone/vector_db.py
+++ src/database/pinecone/vector_db.py
@@ -1,3 +1,4 @@
+import os
 import pinecone
 
 class PineconeVectorDB:
@@ -5,6 +6,12 @@ class PineconeVectorDB:
     manager = pinecone
 
     def connect(self, index_name: str = 'nobrainer'):
+        # Connect Pinecone
+        pinecone.init(
+            api_key=os.getenv("PINECONE_API_KEY"),
+            environment=os.getenv("PINECONE_ENV")
+        )
+
         # Create a Pinecone index
         if index_name not in pinecone.list_indexes():
             pinecone.create_index(index_name, dimension=1536) # set 1536 as the dimension of the embeddings, the default of the text-embedding-ada-002 model
