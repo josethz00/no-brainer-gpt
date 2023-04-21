becfba43b11440c1e0bd890ecedd444ff9fed108
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-21 00:06:18 -0300

    refactor: comment explaining json conversion

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ openai.api_key = os.getenv("OPEN_AI_API_KEY")
 MODEL = "text-embedding-ada-002"
 
 elements = partition_md(filename="./storage/example.md")
-partitioned_text = json.loads(elements_to_json(elements))
+partitioned_text = json.loads(elements_to_json(elements)) # convert partitions into JSON and load into Python dict
 
 filtered_partitioned_text = [element for element in partitioned_text if "text" in element]
