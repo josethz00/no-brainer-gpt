cf6cd25ab490de414819124851659c1de1f0bd3b
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-20 23:01:34 -0300

    feat: generate json partition

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import openai
 import pinecone
 import os
 from dotenv import load_dotenv
+from unstructured.partition.auto import partition
+from unstructured.staging.base import elements_to_json
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPEN_AI_API_KEY")
 MODEL = "text-embedding-ada-002"
+
+elements = partition(filename="./storage/example.md")
+print(elements_to_json(elements))
