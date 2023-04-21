2b88d31b13e115e937e1438f9c07f3b745138ba2
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-21 01:49:14 -0300

    feat: gen chatgpt response

--- main.py
+++ main.py
@@ -31,21 +31,27 @@ partitioned_text = json.loads(elements_to_json(elements)) # convert partitions i
 
 filtered_partitioned_text = [element["text"] for element in partitioned_text if "text" in element]
 
-# Create an embedding for a single document using the text-embedding-ada-002 model
-embeddings_api_response = openai.Embedding.create(
-    input=filtered_partitioned_text,
-    engine=MODEL
-)
-
-embeddings = [record["embedding"] for record in embeddings_api_response["data"]]
-
 batch_size = 500 # set the batch size to 500 because pinecone cannot handle the 1500+ embeddings at once
-
-# Partition the embeddings into batches of 500
-embeddings = np.array_split(embeddings, batch_size)
 # Add the embeddings to the Pinecone index
-for i in range(0, len()
-    to_upsert = zip([uuid.uuid4().hex for _ in embedding], embedding, [element for _ in embedding])
+for i in range(0, len(filtered_partitioned_text), batch_size):
+    # Create an embedding for a single document using the text-embedding-ada-002 model
+    embeddings_api_response = openai.Embedding.create(
+        input=filtered_partitioned_text,
+        engine=MODEL
+    )
+
+    embeddings = [record["embedding"] for record in embeddings_api_response["data"]]
+
+    to_upsert = [
+        {
+            "id": str(uuid.uuid4()),
+            "values": embedding,
+            "metadata": {
+                "text": filtered_partitioned_text[j]
+            }
+        } for j, embedding in enumerate(embeddings)
+    ]
+
     index.upsert(
         vectors=to_upsert,
     )
@@ -59,9 +65,30 @@ query_embedding = openai.Embedding.create(
 # Search in the vector database for the closest vector to the query
 results = index.query(
     [query_embedding],
-    top_k=5,
+    top_k=10,
     include_metadata=True
 )
 
+prompt_gpt = [{'role': 'user', 'content': f"THE QUESTION IS: {query}"}]
+context_gpt = [{'role': 'user', 'content': f"THE CONTEXT IS:"}]
 for match in results['matches']:
-    print(f"{match['score']:.2f}: {match['metadata']['text']}")
+    context_gpt.append({'role': 'user', 'content': f"{match['score']:.3f}: {match['metadata']['text']}"})
+    print(f"{match['score']:.3f}: {match['metadata']['text']}")
+
+prompt_gpt.extend(context_gpt)
+
+# Call ChatGPT to generate a response
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=prompt_gpt,
+    temperature=0.6,
+    max_tokens=2000,
+    top_p=1.0,
+    presence_penalty=0.0,
+)
+
+# Print the response
+print(response)
+
+# delete the index data
+index.delete(delete_all=True)
