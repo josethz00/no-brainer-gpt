26b5c37db88c7114f53e0bbf02b0a1f6a2b5b852
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-23 13:36:24 -0300

    feat(processMdFiles): for loop to process all files'

--- src/core/handlers/md_files/process_md_files.py
+++ src/core/handlers/md_files/process_md_files.py
@@ -8,35 +8,36 @@ from database.pinecone.vector_db import vector_db
 from fastapi import UploadFile
 
 def process_md_files(md_files: list[UploadFile]):
-    openai.api_key = os.getenv("OPEN_AI_API_KEY")
-    MODEL = "text-embedding-ada-002"
+    for md_file in md_files:
+        openai.api_key = os.getenv("OPEN_AI_API_KEY")
+        MODEL = "text-embedding-ada-002"
 
-    elements = partition_md(filename="./storage/example.md")
-    partitioned_text = json.loads(elements_to_json(elements)) # convert partitions into JSON and load into Python dict
+        elements = partition_md(file=md_file) # partition the markdown file
+        partitioned_text = json.loads(elements_to_json(elements)) # convert partitions into JSON and load into Python dict
 
-    filtered_partitioned_text = [element["text"] for element in partitioned_text if "text" in element]
+        filtered_partitioned_text = [element["text"] for element in partitioned_text if "text" in element]
 
-    batch_size = 500 # set the batch size to 500 because pinecone cannot handle the 1500+ embeddings at once
-    # Add the embeddings to the Pinecone index
-    for i in range(0, len(filtered_partitioned_text), batch_size):
-        # Create an embedding for a single document using the text-embedding-ada-002 model
-        embeddings_api_response = openai.Embedding.create(
-            input=filtered_partitioned_text,
-            engine=MODEL
+        batch_size = 500 # set the batch size to 500 because pinecone cannot handle the 1500+ embeddings at once
+        # Add the embeddings to the Pinecone index
+        for i in range(0, len(filtered_partitioned_text), batch_size):
+            # Create an embedding for a single document using the text-embedding-ada-002 model
+            embeddings_api_response = openai.Embedding.create(
+                input=filtered_partitioned_text,
+                engine=MODEL
+            )
+
+        embeddings = [record["embedding"] for record in embeddings_api_response["data"]]
+
+        to_upsert = [
+            {
+                "id": str(uuid.uuid4()),
+                "values": embedding,
+                "metadata": {
+                    "text": filtered_partitioned_text[j]
+                }
+            } for j, embedding in enumerate(embeddings)
+        ]
+
+        vector_db.index.upsert(
+            vectors=to_upsert,
         )
-
-    embeddings = [record["embedding"] for record in embeddings_api_response["data"]]
-
-    to_upsert = [
-        {
-            "id": str(uuid.uuid4()),
-            "values": embedding,
-            "metadata": {
-                "text": filtered_partitioned_text[j]
-            }
-        } for j, embedding in enumerate(embeddings)
-    ]
-
-    vector_db.index.upsert(
-        vectors=to_upsert,
-    )
