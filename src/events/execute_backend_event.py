2aaec6fa05eb5e0c1987732e4da96c0e932ad52a
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-23 22:24:08 -0300

    feat: execute migration on startup

--- src/events/execute_backend_event.py
+++ src/events/execute_backend_event.py
@@ -2,9 +2,11 @@ import typing
 import fastapi
 from database.postgres.sql_db import connect_pg
 from database.pinecone.vector_db import vector_db
+from database.postgres.migrations import _1_create_jobs_table
 
 def execute_backend_server_event_handler(backend_app: fastapi.FastAPI) -> typing.Any:
     async def launch_backend_server_events() -> None:
         vector_db.connect('nobrainer')
         await connect_pg()
+        await _1_create_jobs_table.migrate()
     return launch_backend_server_events
